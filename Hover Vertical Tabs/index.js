f34c309a36c50e96187ff4a661ed8587ea98a257
Md.Khaled Hossain <md.khaled.eee@gmail.com>
2018-11-03 11:57:18 +0600

    hoverable vertical tabs

---
+++ Hover Vertical Tabs/index.js
@@ -0,0 +1,28 @@
+function removeActive() {
+  const tabLinks = document.getElementsByClassName('tablinks');
+
+  [...tabLinks].forEach(tabLink => {
+    tabLink.className = tabLink.className.replace(' active', '');
+  })
+}
+
+function openCity(event, cityName) {
+  // hide all tabcontent
+  const tabContents = document.getElementsByClassName('tabcontent');
+
+  [...tabContents].forEach(tabContent => {
+    tabContent.style.display = "none";
+  });
+  // remove active class
+  removeActive();
+
+  // display tabcontent
+  document.getElementById(cityName).style.display = 'inline-block';
+  // make element active
+  event.currentTarget.className += ' active';
+}
+
+function closeTab(event) {
+  event.currentTarget.parentElement.style.display = 'none';
+  removeActive();
+}
